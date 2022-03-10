5f397654d102042b94bc47b6f12ad713d17f2621
James Stephenson <jstephenson808@gmail.com>
2022-03-10 20:41:22 +0000

    added TEMPLATE_DIR varable, just waiting to see what has been done before I go futher on that

--- wad_2_team_project/settings.py
+++ wad_2_team_project/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 
 # Quick-start development settings - unsuitable for production
