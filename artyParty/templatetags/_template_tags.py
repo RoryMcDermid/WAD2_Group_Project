b8e842d2a6efbb4138bd60b1dcf45c163b4cb9cf
James Stephenson <jstephenson808@gmail.com>
2022-03-10 21:31:47 +0000

    Added template tags but models not done yet so commented out rn

---
+++ artyParty/templatetags/_template_tags.py
@@ -0,0 +1,8 @@
+# from django import template
+# from artyParty.models import Category
+#
+# register = template.Library()
+#
+# @register.inclusion_tag('rango/categories.html')
+# def get_category_list():
+#     return {'categories': Category.objects.all()}
