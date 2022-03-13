be7691c8f0473c07258e1d80122862b3191721b1
Dongni Tao <2552846t@student.gla.ac.uk>
2022-03-13 05:24:19 +0000

    done template

--- artyParty/templatetags/_template_tags.py
+++ artyParty/templatetags/_template_tags.py
@@ -6,3 +6,18 @@
 # @register.inclusion_tag('rango/categories.html')
 # def get_category_list():
 #     return {'categories': Category.objects.all()}
+
+from django import template
+from artyParty.models import Category
+
+register = template.Library()
+
+@register.inclusion_tag('artyParty/get_gallery_list')
+def get_gallery_list():
+     return {'galleries': Gallery.objects.all()，
+        'current_category': current_category}
+            
+
+@register.inclusion_tag('artyParty/get_piece_list')
+def get_piece_list():
+    return {'pieces': Piece.objects.all()}
