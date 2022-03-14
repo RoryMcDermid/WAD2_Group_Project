e05a7fc2e4f8da75dfe9c7f272a982b6073196e3
Julia <2563582p@student.gla.ac.uk>
2022-03-14 00:07:18 +0000

    Models created, Updated admin.py

--- artyParty/admin.py
+++ artyParty/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from artyParty.models import User, Gallery, Piece, Review
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'user_name', 'user_type')
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('gallery_name',)}
+    list_display = ('gallery_id', 'user_id', 'gallery_name')
+
+
+class PieceAdmin(admin.ModelAdmin):
+    list_display = ('piece_id', 'gallery_id', 'piece_name', 'user_id')
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('review_id', 'piece_id', 'rating', 'user_id', 'review')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(Piece, PieceAdmin)
+admin.site.register(Review, ReviewAdmin)
--- artyParty/models.py
+++ artyParty/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class User(models.Model):
+    USER_NAME_MAX_LENGTH = 50
+    USER_TYPE_MAX_LENGTH = 5
+    
+    user_id = models.IntegerField(unique=True, blank=False)
+    user_name = models.CharField(max_length=USER_NAME_MAX_LENGTH, blank=False)
+    user_type = models.CharField(max_length=USER_TYPE_MAX_LENGTH, blank=False)
+
+    def __str__(self):
+        return self.user_id
+
+
+class Gallery(models.Model):
+    GALLERY_NAME_MAX_LENGTH = 50
+
+    gallery_id = models.IntegerField(unique=True, blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    gallery_name = models.CharField(max_length=GALLERY_NAME_MAX_LENGTH, blank=False)
+    slug = models.SlugField(blank=True, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.gallery_name)
+        super(Gallery, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Galleries'
+
+    def __str__(self):
+        return self.gallery_id
+
+
+class Piece(models.Model):
+    PIECE_NAME_MAX_LENGTH = 50
+
+    piece_id = models.IntegerField(unique=True, blank=False)
+    gallery_id = models.ForeignKey(Gallery, on_delete=models.CASCADE)
+    piece_name = models.CharField(max_length=PIECE_NAME_MAX_LENGTH, blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.piece_id
+
+
+class Review(models.Model):
+
+    review_id = models.IntegerField(unique=True, blank=False)
+    piece_id = models.ForeignKey(Piece, on_delete=models.CASCADE, blank=False)
+    rating = models.IntegerField(blank=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    review = models.TextField
+
+    def __str__(self):
+        return self.review_id
