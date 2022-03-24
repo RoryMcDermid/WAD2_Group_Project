fb9aa9cce171168759bb226994a4648a782c7aa3
James Stephenson <jstephenson808@gmail.com>
2022-03-24 09:55:04 +0000

    changed populateUser.py to populate correct fields "django.core.exceptions.FieldError: Cannot resolve keyword 'user_id' into field. Choices are: date_joined, email, first_name, gallery, groups, id, is_active, is_staff, is_superuser, last_login, last_name, logentry, password, piece, review, user_permissions, username, userprofile"

--- populateUser.py
+++ populateUser.py
@@ -6,16 +6,16 @@ from artyParty.models import User
 
 
 def populate():
-    users = [{'user_id': 2563582, 'user_name': 'jnpawlowska123', 'user_type': 'superuser'},
-             {'user_id': 2594321, 'user_name': 'leodastinki', 'user_type': 'superuser'},
-             {'user_id': 3678912, 'user_name': 'vinvangoth', 'user_type': 'superuser'}]
+    users = [{'user_id': 2563582, 'user_name': 'jnpawlowska123', 'user_type': True},
+             {'user_id': 2594321, 'user_name': 'leodastinki', 'user_type': True},
+             {'user_id': 3678912, 'user_name': 'vinvangoth', 'user_type': True}]
 
     for user in users:
         add_user(user['user_id'], user['user_name'], user['user_type'])
 
 
 def add_user(user_id, user_name, user_type):
-    u = User.objects.get_or_create(user_id=user_id, user_name=user_name, user_type=user_type)
+    u = User.objects.get_or_create(id=user_id, username=user_name, is_superuser=user_type)
     return u
 
 
