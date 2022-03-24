486af6bf9f04a3b1d4c8a270ebc9d6d77751599a
James Stephenson <2573758S@student.gla.ac.uk>
2022-03-24 15:47:40 +0000

    deleted bits which didn't work (again)

--- artyParty/views.py
+++ artyParty/views.py
@@ -234,7 +234,7 @@ def show_gallery(request):
     #     pieceDict[piece.gallery_id.gallery_name] = piece
     
     gallery_list = Gallery.objects.order_by("gallery_id") #this 3 needs to be the length of gallery
-    context_dict = {'galleries': gallery_list, 'pieces': pieceDict}
+    context_dict = {'galleries': gallery_list}
     return render(request, 'artyParty/get_gallery_list.html', context=context_dict)
 
 
